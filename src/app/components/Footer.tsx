6ca8c8a9e1d340ae5404f3e912789b6bc83e3ef3
Murilo de Jesus Santos Silva <murilojssilva@outlook.com>
2025-02-19 19:35:41 -0300

    Fix Footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -11,10 +11,12 @@ import Link from 'next/link'
 export default function Footer() {
   return (
     <div className='flex flex-col'>
-      <nav className='bg-gray-50 p-8 flex flex-row justify-between'>
+      <nav className='flex flex-col items-center justify-between sm:justify-between sm:flex-row bg-gray-50 p-8'>
         <div>
-          <h1 className='text-xl font-bold text-blue-800'>Mapa do site</h1>
-          <ul className='flex flex-col py-4 gap-2'>
+          <h1 className='text-xl font-bold text-blue-800 text-center sm:text-left'>
+            Mapa do site
+          </h1>
+          <ul className='flex flex-row sm:flex-col py-4 gap-2'>
             <Link href='' className='text-blue-800 hover:text-blue-600'>
               <li>Quem somos</li>
             </Link>
@@ -27,8 +29,10 @@ export default function Footer() {
           </ul>
         </div>
         <div>
-          <h1 className='text-xl font-bold text-blue-800'>Contato</h1>
-          <ul className='flex flex-col py-4 gap-2'>
+          <h1 className='text-xl font-bold text-blue-800 text-center sm:text-left'>
+            Contato
+          </h1>
+          <ul className='flex flex-row sm:flex-col py-4 gap-2'>
             <Link href='' className='text-blue-800 hover:text-blue-600'>
               <li>E-mail</li>
             </Link>
@@ -41,7 +45,9 @@ export default function Footer() {
           </ul>
         </div>
         <div>
-          <h1 className='text-xl font-bold text-blue-800'>Siga-nos</h1>
+          <h1 className='text-xl font-bold text-blue-800 text-center sm:text-left'>
+            Siga-nos
+          </h1>
           <div className='flex flex-row py-4 gap-2'>
             <div className='rounded-full bg-blue-800 p-2 flex items-center justify-center'>
               <Link href='facebook.com'>
@@ -67,8 +73,10 @@ export default function Footer() {
         </div>
       </nav>
       <div className='bg-gray-900 px-8 py-4 gap-2 flex flex-row justify-between'>
-        <p>Todos os direitos reservados</p>
-        <p>{new Date().getFullYear()} &copy; cryptonews.com.br</p>
+        <p className='text-sm'>Todos os direitos reservados</p>
+        <p className='text-sm'>
+          {new Date().getFullYear()} &copy; cryptonews.com.br
+        </p>
       </div>
     </div>
   )
