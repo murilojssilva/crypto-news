a9c786835d267711370454122399b6bfe186f4d9
Murilo de Jesus Santos Silva <murilojssilva@outlook.com>
2025-02-19 09:11:16 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,29 @@ module.exports = {
       {
         source: '/api/:path*',
         destination:
-          'https://crypto-news-server-d982fcfac1fc.herokuapp.com/posts/:path*',
+          'https://crypto-news-server-d982fcfac1fc.herokuapp.com/:path*', // Removido "posts/" para permitir chamadas dinâmicas
+      },
+    ]
+  },
+
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-Requested-With, Content-Type, Authorization',
+          },
+        ],
       },
     ]
   },
