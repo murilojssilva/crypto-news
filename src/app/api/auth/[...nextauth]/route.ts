0cf33aa11bc9c5c51ac9cb643ab229755046a30f
Murilo de Jesus Santos Silva <murilojssilva@outlook.com>
2025-02-28 22:52:39 -0300

    Types test

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,11 @@
+import { NextApiRequest, NextApiResponse } from 'next'
 import { authConfig } from '../../../../../auth'
 import NextAuth from 'next-auth'
 
-export const GET = (req: any, res: any) => {
+export const GET = (req: NextApiRequest, res: NextApiResponse) => {
   return NextAuth(req, res, authConfig)
 }
 
-export const POST = (req: any, res: any) => {
+export const POST = (req: NextApiRequest, res: NextApiResponse) => {
   return NextAuth(req, res, authConfig)
 }
