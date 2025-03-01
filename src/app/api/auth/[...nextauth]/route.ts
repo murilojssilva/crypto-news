8e06da821c09d016157c055341289387d1792988
Murilo de Jesus Santos Silva <murilojssilva@outlook.com>
2025-03-01 00:08:15 -0300

    Ignore any alerts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import NextAuth from 'next-auth'
 import { authConfig } from '../../../../../auth'
 
-export const GET = (req, res) => NextAuth(req, res, authConfig)
-export const POST = (req, res) => NextAuth(req, res, authConfig)
+export const GET = (req: any, res: any) => NextAuth(req, res, authConfig)
+export const POST = (req: any, res: any) => NextAuth(req, res, authConfig)
