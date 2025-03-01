6a2f7ef0ef0326230638faadcc9840d391eefecf
Murilo de Jesus Santos Silva <murilojssilva@outlook.com>
2025-03-01 00:17:12 -0300

    Fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,14 +7,25 @@ export function middleware(request: NextRequest) {
 
   if (request.nextUrl.pathname.startsWith('/dashboard')) {
     if (!sessionToken) {
+      // Redireciona para a página de login se não estiver autenticado
       const loginUrl = new URL('/login', request.url)
       return NextResponse.redirect(loginUrl)
     }
+  }
+
+  if (
+    (request.nextUrl.pathname.startsWith('/login') ||
+      request.nextUrl.pathname.startsWith('/signup')) &&
+    sessionToken
+  ) {
+    console.log('Usuário já está logado')
+    const dashboardUrl = new URL('/dashboard', request.url)
+    return NextResponse.redirect(dashboardUrl)
   }
 
   return NextResponse.next()
 }
 
 export const config = {
-  matcher: ['/dashboard/:path*'],
+  matcher: ['/dashboard/:path*', '/login', '/signup'],
 }
