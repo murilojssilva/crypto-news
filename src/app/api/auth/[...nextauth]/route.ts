a9aa652706b29c69aa7f8653a67915e4fd419119
Murilo de Jesus Santos Silva <murilojssilva@outlook.com>
2025-02-28 23:18:02 -0300

    Teeeee

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,4 @@
 import NextAuth from 'next-auth'
 import { authConfig } from '../../../../../auth'
-import { NextApiRequest, NextApiResponse } from 'next'
 
-const authHandler = (req: NextApiRequest, res: NextApiResponse) =>
-  NextAuth(req, res, authConfig)
-
-export default authHandler
+export default NextAuth(authConfig)
