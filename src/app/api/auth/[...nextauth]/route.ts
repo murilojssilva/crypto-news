afbc293a53c7009e76c89f8f96a5ec6b0e3156de
Murilo de Jesus Santos Silva <murilojssilva@outlook.com>
2025-02-28 23:27:54 -0300

    TT

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,14 @@
-// src/app/api/auth/[...nextauth]/route.ts
-
 import NextAuth from 'next-auth'
 import { authConfig } from '../../../../../auth'
 import { NextRequest, NextResponse } from 'next/server'
 
+import { NextApiRequest, NextApiResponse } from 'next'
+
 export async function POST(req: NextRequest, res: NextResponse) {
   try {
-    // Aqui, usamos NextAuth para lidar com a autenticação
-    return await NextAuth(req, res, authConfig)
+    const nextApiReq = req as unknown as NextApiRequest
+    const nextApiRes = res as unknown as NextApiResponse
+    return await NextAuth(nextApiReq, nextApiRes, authConfig)
   } catch (error) {
     console.error(error)
     return new NextResponse('Internal Server Error', { status: 500 })
@@ -16,8 +17,9 @@ export async function POST(req: NextRequest, res: NextResponse) {
 
 export async function GET(req: NextRequest, res: NextResponse) {
   try {
-    // O mesmo vale para o GET, que pode ser usado para obter informações de sessão, por exemplo.
-    return await NextAuth(req, res, authConfig)
+    const nextApiReq = req as unknown as NextApiRequest
+    const nextApiRes = res as unknown as NextApiResponse
+    return await NextAuth(nextApiReq, nextApiRes, authConfig)
   } catch (error) {
     console.error(error)
     return new NextResponse('Internal Server Error', { status: 500 })
