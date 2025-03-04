9c70fa2d5288b41c016fce76502070809ee43440
Murilo de Jesus Santos Silva <murilojssilva@outlook.com>
2025-03-04 19:00:15 -0300

    Update types in PUT

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -28,10 +28,7 @@ export async function GET(req: Request, { params }: { params: any }) {
   }
 }
 
-export async function PUT(
-  req: Request,
-  { params }: { params: { id: string } }
-) {
+export async function PUT(req: Request, { params }: { params: any }) {
   try {
     const { id } = params
     const { firstName, lastName, updatedAt, password } = await req.json()
