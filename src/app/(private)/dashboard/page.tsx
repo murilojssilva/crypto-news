52e54309aac93cfe573aef3828c532bf657aaf27
Murilo de Jesus Santos Silva <murilojssilva@outlook.com>
2025-03-04 22:21:35 -0300

    Update Dashboard View

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -5,10 +5,14 @@ import HeaderDashboard from '@/app/components/Dashboard/Header'
 import Sidebar from '@/app/components/Sidebar'
 import { useSession } from 'next-auth/react'
 import { useFormattedDate } from '@/hooks/useFormatted'
+import { usePosts } from '@/context/PostContext'
+import { Title } from '@/app/components/Dashboard/Title'
 
 export default function Dashboard() {
   const { data: session } = useSession()
   const currentDate = useFormattedDate()
+
+  const { posts } = usePosts()
   return (
     <div className='bg-gray-50 pb-4 h-screen flex'>
       <Sidebar />
@@ -21,8 +25,9 @@ export default function Dashboard() {
           title='Dashboard'
         />
 
-        <div className='p-4 mb-4'>
-          <div className='flex flex-row gap-4 justify-center'>
+        <section className='p-6 grid gap-4'>
+          <Title title='Dashboard' />
+          <div className='grid grid-cols-2 gap-x-4 gap-y-8'>
             <div className='border border-gray-500 p-4 rounded-md flex flex-col gap-3 justify-around'>
               <div>
                 <h3 className='text-gray-800 text-xl font-bold'>
@@ -31,7 +36,77 @@ export default function Dashboard() {
               </div>
               <div className='flex flex-row justify-between'>
                 <Pen color='black' />
-                <span className='text-blue-800 font-bold text-3xl'>9</span>
+                <span className='text-blue-800 font-bold text-3xl'>
+                  {posts.length}
+                </span>
+              </div>
+            </div>
+            <div className='border border-gray-500 p-4 rounded-md flex flex-col gap-3 justify-around'>
+              <div>
+                <h3 className='text-gray-800 text-xl font-bold'>
+                  Número de posts
+                </h3>
+              </div>
+              <div className='flex flex-row justify-between'>
+                <Pen color='black' />
+                <span className='text-blue-800 font-bold text-3xl'>
+                  {posts.length}
+                </span>
+              </div>
+            </div>
+            <div className='border border-gray-500 p-4 rounded-md flex flex-col gap-3 justify-around'>
+              <div>
+                <h3 className='text-gray-800 text-xl font-bold'>
+                  Quantidade de likes
+                </h3>
+              </div>
+              <div className='flex flex-row justify-between'>
+                <Pen color='black' />
+                <span className='text-blue-800 font-bold text-3xl'>4000</span>
+              </div>
+            </div>
+            <div className='border border-gray-500 p-4 rounded-md flex flex-col gap-3 justify-around'>
+              <div>
+                <h3 className='text-gray-800 text-xl font-bold'>
+                  Quantidade de likes
+                </h3>
+              </div>
+              <div className='flex flex-row justify-between'>
+                <Pen color='black' />
+                <span className='text-blue-800 font-bold text-3xl'>4000</span>
+              </div>
+            </div>
+            <div className='border border-gray-500 p-4 rounded-md flex flex-col gap-3 justify-around'>
+              <div>
+                <h3 className='text-gray-800 text-xl font-bold'>
+                  Quantidade de likes
+                </h3>
+              </div>
+              <div className='flex flex-row justify-between'>
+                <Pen color='black' />
+                <span className='text-blue-800 font-bold text-3xl'>4000</span>
+              </div>
+            </div>
+            <div className='border border-gray-500 p-4 rounded-md flex flex-col gap-3 justify-around'>
+              <div>
+                <h3 className='text-gray-800 text-xl font-bold'>
+                  Quantidade de likes
+                </h3>
+              </div>
+              <div className='flex flex-row justify-between'>
+                <Pen color='black' />
+                <span className='text-blue-800 font-bold text-3xl'>4000</span>
+              </div>
+            </div>
+            <div className='border border-gray-500 p-4 rounded-md flex flex-col gap-3 justify-around'>
+              <div>
+                <h3 className='text-gray-800 text-xl font-bold'>
+                  Quantidade de likes
+                </h3>
+              </div>
+              <div className='flex flex-row justify-between'>
+                <Pen color='black' />
+                <span className='text-blue-800 font-bold text-3xl'>4000</span>
               </div>
             </div>
             <div className='border border-gray-500 p-4 rounded-md flex flex-col gap-3 justify-around'>
@@ -46,7 +121,7 @@ export default function Dashboard() {
               </div>
             </div>
           </div>
-        </div>
+        </section>
       </div>
     </div>
   )
