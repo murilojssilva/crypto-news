6429a6e554cb043f7946c44a5456e5e4be2c40e7
Murilo de Jesus Santos Silva <murilojssilva@outlook.com>
2025-03-06 20:11:30 -0300

    Fix db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,9 @@
 import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient }
+const globalForPrisma = global as unknown as { prisma?: PrismaClient }
 
-const db = globalForPrisma.prisma ?? new PrismaClient()
+export const db = globalForPrisma.prisma ?? new PrismaClient()
 
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = db
-}
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
 
 export default db
