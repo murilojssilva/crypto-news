f398f2a96e37caf35095889be06ca28c7b67b28f
Murilo de Jesus Santos Silva <murilojssilva@outlook.com>
2025-03-07 22:12:34 -0300

    Fix getUsers

--- src/lib/users.ts
+++ src/lib/users.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const getUsers = async () => {
-  const response = await axios.get('/api/posts')
+  const response = await axios.get('/api/users')
   return response.data
 }
